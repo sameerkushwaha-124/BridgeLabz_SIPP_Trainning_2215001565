17a5f90aff98773131a9395df7af105767f8ec47
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-06-11 10:23:37 +0530

    first program

---
+++ Level_1/Level1P1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Level1P1{
+   public static void main(String args[]){
+       int birthYear = 2000;
+       int currYear  = 2024;
+       System.out.println("Harry's age in 2024 is : " + (currYear-birthYear));
+  
+   }
+}
