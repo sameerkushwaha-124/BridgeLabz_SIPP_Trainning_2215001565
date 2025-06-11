aa5c06366f054ccbda31c9724ad6657233b254db
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-06-11 10:52:50 +0530

    problem 2

---
+++ Level_1/Level1P2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Level1P2{
+   public static void main(String args[]){
+       int maths = 94;
+       int physics  = 95;
+       int chemistry = 96;
+       System.out.println("Sam’s average mark in PCM is: " + (maths + physics + chemistry)/3 + "%");
+   }
+}
