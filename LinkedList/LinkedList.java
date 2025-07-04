8ef456e049e63eccb214ee4b7794323b047b9d60
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-07-04 11:26:55 +0530

    [Sameer Kushwaha] Add . code to commited for LinkedList and ParcelTracking system

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -10,6 +10,11 @@ class ListNode {
 public class LinkedList {
 
     private ListNode head; // Declare the head pointer
+    public void addFirst(int val){
+        ListNode newNode = new ListNode(val);
+        newNode.next = head;
+        head = newNode;        
+    }
 
     public void insert(int val) {
         ListNode newNode = new ListNode(val);
@@ -32,12 +37,29 @@ public class LinkedList {
         }
         System.out.println();
     }
+    public void delete(int val){
+        ListNode curr = head;
+        while(curr.next != null && curr.next.val != val){
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        
+        // System.out.println(curr.val);
+    }
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         for (int i = 0; i < 10; i++) {
             ll.insert(i);
         }
+        
         ll.print();
+        ll.addFirst(90);
+        ll.print();
+        ll.delete(5);
+        ll.print();
+        ll.delete(9);
+        ll.print();
+
     }
 }
