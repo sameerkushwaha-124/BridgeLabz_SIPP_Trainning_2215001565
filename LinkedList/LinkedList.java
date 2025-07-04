7238cf8247bff1221df239d50f166110991e7211
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-07-04 09:55:27 +0530

    [Sameer Kushwaha] Add . Code to be update for the implementation of LinkedList

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,43 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class LinkedList {
+
+    private ListNode head; // Declare the head pointer
+
+    public void insert(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void print() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        for (int i = 0; i < 10; i++) {
+            ll.insert(i);
+        }
+        ll.print();
+    }
+}
